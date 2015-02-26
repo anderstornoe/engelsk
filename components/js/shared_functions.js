a6fef6369e68c147329f3f6abf06c65dfa2e61f2
Thomas <kvucthan@gmail.com>
2015-02-26 11:56:40 +0100

    Shared functions er blevet wrappet i en klasse

--- components/js/shared_functions.js
+++ components/js/shared_functions.js
@@ -1,16 +1,32 @@
-// funktion der laver et overlay med information / valg, der forsvinder ved klik hvorsom helst
-// parametre: 
-function overlayInfo(title, content, buttontxt) {}
+// For at undgaa sammenfald i mellem funktionsnavne i concatede .js-filer, saa wrappes
+// alle funktioner i en "klasse" som hedder "kvuc_js_class" (navnet er ligegyldigt, det skal bare 
+// vaere et unikt navn).
+//
+// For at anvende en funktion inde i klassen, skal et objektet foerst "instiantieres" fra 
+// "klassen" - nedenstaaende linje skal kunskrive een gang:
+//
+// 				var kvuc = Object.create(kvuc_js_class);
+//
+// For at anvende funktionen "overlayInfo(title, content, buttontxt)" skrives:
+// 
+// 				kvuc.overlayInfo(title, content, buttontxt);
+//
 
-//funktion der blander et div_Array
+var kvuc_js_class = {
 
-//funktion der shuffler et Array 
+	// funktion der laver et overlay med information / valg, der forsvinder ved klik hvorsom helst
+	// parametre: 
+	function overlayInfo(title, content, buttontxt) {}
 
-// Dialog box
+	//funktion der blander et div_Array
 
-function dialogBox() {
+	//funktion der shuffler et Array 
+
+	// Dialog box
+
+	function dialogBox() {
+
+	}
 
 }
-
-
 
