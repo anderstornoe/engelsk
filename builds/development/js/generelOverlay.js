d40770552a6092d8418b29df2c9b0f5f583e3dd4
Anders Tornøe <ato@kvuc.dk>
2015-04-09 11:15:37 +0200

    Css update osv

--- builds/development/js/generelOverlay.js
+++ builds/development/js/generelOverlay.js
@@ -23,8 +23,8 @@ var GeneralOverlayClass = {
 
 
 	ButtonControler : 	'<div id="OverlyWhyHowContainer">' + 
-							'<a href="#" id="OverlayHow" class="OverlayButton btn btn-default btn-lg"> HOW </a>' + 
-							'<a href="#" id="OverlayWhy" class="OverlayButton btn btn-default btn-lg"> WHY </a>' + 
+							'<a href="#" id="OverlayHow" class="OverlayButton btn btn-default"> HOW </a>' + 
+							'<a href="#" id="OverlayWhy" class="OverlayButton btn btn-default"> WHY </a>' + 
 					 	'</div>',
 
 	OverlayMarkup : '<div class="Overlay"></div>' + 
--- builds/development/js/vid_new.js
+++ builds/development/js/vid_new.js
@@ -209,7 +209,7 @@ function introscreen() {
     player.pauseVideo();
 
     $("#overlay").fadeIn(1000);
-    $("#overlay").append("<div class='intro'><div class='h1'>" + intro_header + "</div><p class='feed_txt'>" + intro_text + "</p><div class='btn btn-default btn-lg introknap'>" + intro_knap + "</div></div>");
+    $("#overlay").append("<div class='intro'><div class='movie_h1'>" + intro_header + "</div><p class='feed_txt'>" + intro_text + "</p><div class='btn btn-default btn-lg introknap'>" + intro_knap + "</div></div>");
     $("#overlay").click(function() {
 
         $(this).fadeOut(1000, function() {
